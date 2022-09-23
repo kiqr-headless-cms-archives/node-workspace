5ddc6efcc0f49961d6fc9e75e860f0f638c8e07b
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2022-09-23 23:01:48 +0200

    * [cloud-editor] check for array type before .filter
    * [cloud-editor] check for array type before .filter
    
    * chore: move .vscode to monorepo root
    
    * [react] fix: remove margin bottom from Heading
    
    * [cloud-editor] chore: styles and lint

--- apps/cloud-editor/src/pages/[projectId]/[environmentId]/collections/[contentTypeId]/index.tsx
+++ apps/cloud-editor/src/pages/[projectId]/[environmentId]/collections/[contentTypeId]/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 
-import { Heading } from '@kiqr/react'
+import { Card, Heading } from '@kiqr/react'
 
 import { PageTitle } from '../../../../../components'
 import { useCurrent } from '../../../../../hooks'
@@ -25,17 +25,34 @@ const ContentTypePage: NextPage = () => {
         }
       />
 
-      <div className="flex items-center h-12 bg-primary-700 p-2 text-xs gap-x-2 rounded">
-        <div className="bg-primary-800 text-white mr-1 w-8 h-8 rounded flex items-center justify-center">
+      {/*<div className="flex items-center bg-primary-800 p-2 text-xs gap-x-2 rounded">
+        <div className="bg-primary-900 text-white mr-1 w-8 h-8 rounded flex items-center justify-center">
           <FaLink />
         </div>
         <strong className="text-white">GET</strong>
         <a target="_blank" className="text-white" href="#">
           {`https://content.kiqr.cloud/v1/${currentProject?.slug}/collections/${query?.contentTypeId}`}
         </a>
-      </div>
+      </div>*/}
 
-      <pre>{JSON.stringify(currentContentType, null, 2)}</pre>
+      <Card
+        title="All"
+        subtitle={`Displaying ${0} of ${0} resources in this collection.`}
+      >
+        <table className="table border-t">
+          <thead>
+            <tr>
+              <th className="mini"></th>
+              <th>Name</th>
+              <th>Updated at</th>
+              <th>Created at</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+          </table>
+      </Card>
     </>
   )
 }
--- sdks/react/src/components/atoms/Heading/index.tsx
+++ sdks/react/src/components/atoms/Heading/index.tsx
@@ -10,7 +10,7 @@ export interface PageHeaderProps {
 
 export const Heading = ({ title, subtitle }: PageHeaderProps): ReactElement => {
   return (
-    <header className="flex flex-col gap-y-1 mb-5">
+    <header className="flex flex-col gap-y-1">
       <h1 className="text-xl font-bold text-primary-700">
         {title || <Placeholder />}
       </h1>
