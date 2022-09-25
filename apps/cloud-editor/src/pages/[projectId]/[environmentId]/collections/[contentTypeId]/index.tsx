14d6dd84487cc59d0dd396e197588fb40b9c1245
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2022-09-25 11:51:58 +0200

    [cloud-editor] feat: "getting started"-box when no resources (#16)

--- apps/cloud-editor/src/pages/[projectId]/[environmentId]/collections/[contentTypeId]/index.tsx
+++ apps/cloud-editor/src/pages/[projectId]/[environmentId]/collections/[contentTypeId]/index.tsx
@@ -1,12 +1,24 @@
 import type { NextPage } from 'next'
 
-import { Card, Heading } from '@kiqr/react'
-
+import { Box, Button, Card, Heading } from '@kiqr/react'
 import { PageTitle } from '../../../../../components'
 import { useCurrent } from '../../../../../hooks'
 
+import Image from 'next/image'
+import inflection from 'inflection'
+
 const ContentTypePage: NextPage = () => {
   const { currentContentType, currentProject } = useCurrent()
+  
+  const resources: string[] = []
+
+  const singularizedContentTypeName = currentContentType
+    ? inflection.transform(currentContentType?.name, ['singularize'])?.toLowerCase()
+    : null
+
+  const pluralizedContentTypeName = currentContentType
+    ? inflection.transform(currentContentType?.name, ['pluralize'])?.toLowerCase()
+    : null
 
   return (
     <>
@@ -32,23 +44,68 @@ const ContentTypePage: NextPage = () => {
         </a>
       </div>*/}
 
-      <Card
-        title="All"
-        subtitle={`Displaying ${0} of ${0} resources in this collection.`}
-      >
-        <table className="table border-t">
-          <thead>
-            <tr>
-              <th className="mini"></th>
-              <th>Name</th>
-              <th>Updated at</th>
-              <th>Created at</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </table>
-      </Card>
+      {currentContentType ? (
+        <Box>
+          <div className="container mx-auto py-24">
+            <div className="flex flex-wrap">
+              <div className="w-full md:w-1/2">
+                <div className="max-w-md mx-auto">
+                  <h2 className="mb-8 text-4xl md:text-5xl font-heading font-bold text-neutral-900 md:leading-15">
+                    Get started now by creating your first{' '}
+                    <span className="text-primary-700">
+                      {singularizedContentTypeName}
+                    </span>
+                    .
+                  </h2>
+                  <p className="text-lg text-slate-400 mb-8">
+                    We couldn&apos;t find any{' '}
+                    <strong>{pluralizedContentTypeName}</strong> in the{' '}
+                    database. Get started now by creating a new{' '}
+                    {singularizedContentTypeName}{' '}
+                    or import a collection of {pluralizedContentTypeName}.
+                  </p>
+                  <div className="gap-x-5 flex">
+                    <Button
+                      text={`Create ${singularizedContentTypeName}`}
+                      type="primary"
+                      size="lg"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 flex pr-20">
+                <div className="flex relative flex-1 items-center justify-center">
+                  <Image
+                    src="/assets/images/undraw_elements.svg"
+                    alt=""
+                    layout="fill"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Box>
+      ) : null}
+
+      {resources && resources?.length > 0 ? (
+        <Card
+          title="All"
+          subtitle={`Displaying ${0} of ${0} resources in this collection.`}
+        >
+          <table className="table border-t">
+            <thead>
+              <tr>
+                <th className="mini"></th>
+                <th>Name</th>
+                <th>Updated at</th>
+                <th>Created at</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </Card>
+      ) : null}
     </>
   )
 }
