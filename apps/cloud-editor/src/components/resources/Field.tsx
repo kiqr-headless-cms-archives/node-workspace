5151652b8556effa2e61185a3cb6aa3f44d2a9a6
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2022-10-20 23:54:00 +0200

    feat(cloud-editor): textarea field (#67)

--- apps/cloud-editor/src/components/resources/Field.tsx
+++ apps/cloud-editor/src/components/resources/Field.tsx
@@ -8,7 +8,59 @@ export interface FieldProps {
   errors: any
 }
 
-export const Field = ({ field, register, errors }: FieldProps): JSX.Element => {
+const StringField = (props: FieldProps): JSX.Element => {
+  const { field, register, errors } = props
+
+  return (
+    <>
+      <input
+        type="text"
+        {...register(`content[${field.id}]`, { required: true })}
+        className="border-neutral-200 outline-none focus:ring-0 text-sm bg-white"
+        placeholder={`Enter a value for "${field.label.toLowerCase()}"`}
+      />
+
+      {errors?.content?.[field.id] && (
+        <FormError message="This field is required" />
+      )}
+    </>
+  )
+}
+
+const TextareaField = (props: FieldProps): JSX.Element => {
+  const { field, register, errors } = props
+
+  return (
+    <>
+      <textarea
+        type="text"
+        rows="3"
+        {...register(`content[${field.id}]`, { required: true })}
+        className="border-neutral-200 outline-none focus:ring-0 text-sm bg-white"
+        placeholder={`Enter a text for "${field.label.toLowerCase()}"`}
+      ></textarea>
+
+      {errors?.content?.[field.id] && (
+        <FormError message="This field is required" />
+      )}
+    </>
+  )
+}
+
+export const Field = (props: FieldProps): JSX.Element => {
+  const { field } = props
+
+  const renderField = (type: string) => {
+    switch (type) {
+      case 'string':
+        return <StringField {...props} />
+      case 'text':
+        return <TextareaField {...props} />
+      default:
+        return `Unknown field type "${field.type}"`
+    }
+  }
+
   return (
     <div
       key={field.id}
@@ -16,21 +68,14 @@ export const Field = ({ field, register, errors }: FieldProps): JSX.Element => {
     >
       <label
         htmlFor={field.id}
-        className={`text-primary-700 text-xs mb-2 uppercase ${
+        className={`text-primary-700 text-xs mb-3 uppercase ${
           field.required ? 'font-bold' : null
         }`}
       >
         {field.label}
       </label>
-      <input
-        type="text"
-        {...register(`content[${field.id}]`, { required: true })}
-        className="border-neutral-200 outline-none focus:ring-0 text-sm bg-white"
-        placeholder={`Enter a value for "${field.label.toLowerCase()}"`}
-      />
-      {errors?.content?.[field.id] && (
-        <FormError message="This field is required" />
-      )}
+
+      {renderField(field.type)}
     </div>
   )
 }
